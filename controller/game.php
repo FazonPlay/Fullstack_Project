9c0c55f8e3e35dcc8958bdb82b52ea775cfbbe81
FazonPlay <david.gevorgian69@gmail.com>
2025-01-19 22:52:49 +0100

    minor changes again

--- controller/game.php
+++ controller/game.php
@@ -1,3 +1,9 @@
 <?php
 require "model/game.php";
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+    $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest'
+) {
+
+}
+
 require "view/game.php";
--- view/game.php
+++ view/game.php
@@ -1,16 +1,27 @@
-<?php
-?>
 <div class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h1>Memory Game</h1>
         <div id="timer" class="text-danger fw-bold">Time: 05:00</div>
     </div>
     <div id="game-board" class="row row-cols-4 g-3">
-        <!-- Cards will be dynamically inserted using JavaScript -->
+        <!-- Cards will be dynamically added here by JavaScript -->
     </div>
 </div>
+
 <script src="./assets/js/game.js" type="module"></script>
 <script type="module">
+    // import {Game} from "./assets/js/game.js";
+
     document.addEventListener('DOMContentLoaded', () => {
+        let cards = [];
+        let flippedCards = [];
+        let matches = 0;
+        const totalPairs = 8;
+        let timer;
+        let gameBoard;
+        let timerDisplay;
+
+
     });
+
 </script>
