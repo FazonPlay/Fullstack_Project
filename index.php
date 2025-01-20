fcccd2d20bdb5a0ebb8320932d76e3ad4cc49559
David <david.gevorgian69@gmail.com>
2025-01-20 15:26:10 +0100

    secure database working

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
+require __DIR__ . "/vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 include 'includes/database.php';
+require ("includes/helper.php");
+
 
 if (isset($_GET['disconnect'])) {
     session_destroy();
