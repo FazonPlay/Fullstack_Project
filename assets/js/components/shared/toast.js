6f830bc83531b32ea224299e66d26245700242f3
FazonPlay <david.gevorgian69@gmail.com>
2025-02-02 15:45:22 +0100

    toast fix

--- assets/js/components/shared/toast.js
+++ assets/js/components/shared/toast.js
@@ -1,13 +1,18 @@
 export const showToast = (message, color) => {
-    const toastElement = document.querySelector('#toast')
+    const toastElement = document.querySelector('#toast');
+    const toastBody = toastElement.querySelector('.toast-body');
+
     const toast = new bootstrap.Toast(toastElement, {
         delay: 5000
-    })
-    toastElement.classList.remove('bg-danger','bg-success')
-    toastElement.classList.add(color)
+    });
 
-    toastElement.querySelector('.toast-body').innerHTML = message
+    // Reset classes
+    toastElement.classList.remove('bg-danger', 'bg-success');
 
-    toast.show()
+    // Add new classes
+    toastElement.classList.add(color === 'bg-danger' ? 'bg-danger' : 'bg-success');
+    toastBody.classList.add('text-white');
 
-}
+    toastBody.innerHTML = message;
+    toast.show();
+};
