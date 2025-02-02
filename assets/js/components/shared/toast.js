8da034f33139ebef1e7b2b45ac0caf19a02a5c2c
FazonPlay <david.gevorgian69@gmail.com>
2025-02-02 15:45:39 +0100

    toast fix

--- assets/js/components/shared/toast.js
+++ assets/js/components/shared/toast.js
@@ -6,10 +6,8 @@ export const showToast = (message, color) => {
         delay: 5000
     });
 
-    // Reset classes
     toastElement.classList.remove('bg-danger', 'bg-success');
 
-    // Add new classes
     toastElement.classList.add(color === 'bg-danger' ? 'bg-danger' : 'bg-success');
     toastBody.classList.add('text-white');
 
